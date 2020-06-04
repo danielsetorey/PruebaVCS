3f431b92dc4ca630fd185aad200913e061bd741a
danielsetoubu <dsr0018@alu.ubu.es>
2020-06-04 09:37:31 +0200

    Añadido sistema de documentación

--- test_pruebas.py
+++ test_pruebas.py
@@ -4,6 +4,7 @@ from pruebas import suma
 
 class TestSuma(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(suma(2, 2), 4)
+        assert suma(2, 2) == 4
 
-
+    def test_suma2(self):
+        assert suma(3, 2) == 5
