a0ce32d005b7e7f584205b9b8b0dcf7c90451e90
danielsetoubu <dsr0018@alu.ubu.es>
2020-06-04 09:46:43 +0200

    Prueba de integración con Travis

--- test_pruebas.py
+++ test_pruebas.py
@@ -1,12 +1,14 @@
-import unittest
+import pytest
 from pruebas import suma
 
 
-class TestSuma(unittest.TestCase):
+class TestSuma():
     def test_suma(self):
         assert suma(2, 2) == 4
 
     def test_suma2(self):
         assert suma(3, 2) == 5
 
+    def test_suma3(self):
+        assert suma(4, 2) == 6
 
